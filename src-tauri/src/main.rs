66d78f3a858048be7d2e391522518cb553837330
Gizmo <cunyu@liuli.lol>
2022-06-07 00:29:08 +0800

    feat: 未安装Runtime自动跳转到下载地址

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,22 +1,36 @@
 #![cfg_attr(
-    all(not(debug_assertions), target_os = "windows"),
-    windows_subsystem = "windows"
+  all(not(debug_assertions), target_os = "windows"),
+  windows_subsystem = "windows"
 )]
+
+const WEBVIEW2_DOWNLOAD_URL: &str =
+  "https://developer.microsoft.com/microsoft-edge/webview2#download-section";
 
 mod vrchat;
 
 fn main() {
-    tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![
-            vrchat::vrchat_path,
-            vrchat::vrchat_config,
-            vrchat::save_config,
-            vrchat::total_cache,
-            vrchat::move_cache,
-            vrchat::remove_cache,
-            vrchat::open_vrchat_path,
-            vrchat::check_new_path,
-        ])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+  match tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![
+      vrchat::vrchat_path,
+      vrchat::vrchat_config,
+      vrchat::save_config,
+      vrchat::total_cache,
+      vrchat::move_cache,
+      vrchat::remove_cache,
+      vrchat::open_vrchat_path,
+      vrchat::check_new_path,
+    ])
+    .run(tauri::generate_context!())
+  {
+    Ok(_) => (),
+    Err(e) => {
+      if e.to_string().contains("WebView2") {
+        std::process::Command::new("cmd.exe")
+          .args(&["/C", "start", WEBVIEW2_DOWNLOAD_URL])
+          .spawn()
+          .unwrap();
+      }
+      std::process::exit(1);
+    }
+  }
 }
--- src-tauri/src/vrchat.rs
+++ src-tauri/src/vrchat.rs
@@ -6,113 +6,113 @@ use std::{env, path::Path};
 #[tauri::command]
 #[cfg(target_os = "windows")]
 pub fn vrchat_path() -> Option<String> {
-    let _vrchat_path = match env::var("LOCALAPPDATA").or_else(|_| env::var("APPDATA")) {
-        Ok(path) => Path::new(path.as_str()).join("..//LocalLow//VRChat//VRChat"),
-        Err(_) => return None,
-    };
-    Some(
-        _vrchat_path
-            .absolutize()
-            .unwrap()
-            .to_str()
-            .unwrap()
-            .to_string(),
-    )
+  let _vrchat_path = match env::var("LOCALAPPDATA").or_else(|_| env::var("APPDATA")) {
+    Ok(path) => Path::new(path.as_str()).join("..//LocalLow//VRChat//VRChat"),
+    Err(_) => return None,
+  };
+  Some(
+    _vrchat_path
+      .absolutize()
+      .unwrap()
+      .to_str()
+      .unwrap()
+      .to_string(),
+  )
 }
 
 #[tauri::command]
 pub fn vrchat_config() -> Result<String, tauri::InvokeError> {
-    if let Some(_vrchat_path) = vrchat_path() {
-        let config_path = Path::new(_vrchat_path.as_str()).join("config.json");
-        if !config_path.exists() {
-            return Ok("{}".to_string());
-        }
-        return match fs::read_to_string(config_path) {
-            Ok(config) => Ok(config),
-            Err(err) => Err(tauri::InvokeError::from(err.to_string())),
-        };
+  if let Some(_vrchat_path) = vrchat_path() {
+    let config_path = Path::new(_vrchat_path.as_str()).join("config.json");
+    if !config_path.exists() {
+      return Ok("{}".to_string());
     }
-    Err(tauri::InvokeError::from("vrchat-path-notfound"))
+    return match fs::read_to_string(config_path) {
+      Ok(config) => Ok(config),
+      Err(err) => Err(tauri::InvokeError::from(err.to_string())),
+    };
+  }
+  Err(tauri::InvokeError::from("vrchat-path-notfound"))
 }
 
 #[tauri::command]
 pub fn total_cache() -> Option<String> {
-    if let Some(_vrchat_path) = vrchat_path() {
-        let cache_path = Path::new(_vrchat_path.as_str()).join("Cache-WindowsPlayer");
-        let total_size = fs_extra::dir::get_size(cache_path).ok()?;
-        return Some(ByteSize::b(total_size).to_string_as(true));
-    }
-    None
+  if let Some(_vrchat_path) = vrchat_path() {
+    let cache_path = Path::new(_vrchat_path.as_str()).join("Cache-WindowsPlayer");
+    let total_size = fs_extra::dir::get_size(cache_path).ok()?;
+    return Some(ByteSize::b(total_size).to_string_as(true));
+  }
+  None
 }
 
 #[tauri::command]
 pub fn move_cache(new_path: &str) -> Result<(), tauri::InvokeError> {
-    match vrchat_path() {
-        Some(_vrchat_path) => {
-            let cache_path = Path::new(_vrchat_path.as_str()).join("Cache-WindowsPlayer");
-            if !cache_path.exists() {
-                return Err(tauri::InvokeError::from("cache-directory-notfound"));
-            }
-            if let Err(err) = check_new_path(new_path) {
-                return Err(err);
-            }
-            let _new_path = new_path.to_string();
-            std::thread::spawn(move || {
-                fs_extra::dir::move_dir_with_progress(
-                    cache_path,
-                    _new_path,
-                    &fs_extra::dir::CopyOptions::new(),
-                    |process_info| {
-                        println!("{}", process_info.total_bytes);
-                        fs_extra::dir::TransitProcessResult::ContinueOrAbort
-                    },
-                )
-                .unwrap();
-            });
-            Ok(())
-        }
-        None => Err(tauri::InvokeError::from("vrchat-path-notfound")),
+  match vrchat_path() {
+    Some(_vrchat_path) => {
+      let cache_path = Path::new(_vrchat_path.as_str()).join("Cache-WindowsPlayer");
+      if !cache_path.exists() {
+        return Err(tauri::InvokeError::from("cache-directory-notfound"));
+      }
+      if let Err(err) = check_new_path(new_path) {
+        return Err(err);
+      }
+      let _new_path = new_path.to_string();
+      std::thread::spawn(move || {
+        fs_extra::dir::move_dir_with_progress(
+          cache_path,
+          _new_path,
+          &fs_extra::dir::CopyOptions::new(),
+          |process_info| {
+            println!("{}", process_info.total_bytes);
+            fs_extra::dir::TransitProcessResult::ContinueOrAbort
+          },
+        )
+        .unwrap();
+      });
+      Ok(())
     }
+    None => Err(tauri::InvokeError::from("vrchat-path-notfound")),
+  }
 }
 
 #[tauri::command]
 pub fn remove_cache() {
-    if let Some(_vrchat_path) = vrchat_path() {
-        let cache_path = Path::new(_vrchat_path.as_str()).join("Cache-WindowsPlayer");
-        if cache_path.exists() {
-            fs_extra::dir::remove(cache_path).ok();
-        }
+  if let Some(_vrchat_path) = vrchat_path() {
+    let cache_path = Path::new(_vrchat_path.as_str()).join("Cache-WindowsPlayer");
+    if cache_path.exists() {
+      fs_extra::dir::remove(cache_path).ok();
     }
+  }
 }
 
 #[tauri::command]
 #[cfg(target_os = "windows")]
 pub fn open_vrchat_path() {
-    if let Some(_vrchat_path) = vrchat_path() {
-        std::process::Command::new("cmd.exe")
-            .args(&["/C", "start", _vrchat_path.as_str()])
-            .spawn()
-            .ok();
-    }
+  if let Some(_vrchat_path) = vrchat_path() {
+    std::process::Command::new("cmd.exe")
+      .args(&["/C", "start", _vrchat_path.as_str()])
+      .spawn()
+      .ok();
+  }
 }
 
 #[tauri::command]
 pub fn save_config(config: &str) -> Result<(), tauri::InvokeError> {
-    if let Some(_vrchat_path) = vrchat_path() {
-        let config_path = Path::new(_vrchat_path.as_str()).join("config.json");
-        fs::write(config_path, config).ok();
-        return Ok(());
-    }
-    Err(tauri::InvokeError::from("vrchat-path-notfound"))
+  if let Some(_vrchat_path) = vrchat_path() {
+    let config_path = Path::new(_vrchat_path.as_str()).join("config.json");
+    fs::write(config_path, config).ok();
+    return Ok(());
+  }
+  Err(tauri::InvokeError::from("vrchat-path-notfound"))
 }
 
 #[tauri::command]
 pub fn check_new_path(new_path: &str) -> Result<(), tauri::InvokeError> {
-    let _new_path = Path::new(new_path);
-    if !_new_path.exists() {
-        return Err(tauri::InvokeError::from("target-directory-not-exist"));
-    } else if !_new_path.is_dir() {
-        return Err(tauri::InvokeError::from("target-directory-not-directory"));
-    }
-    return Ok(());
+  let _new_path = Path::new(new_path);
+  if !_new_path.exists() {
+    return Err(tauri::InvokeError::from("target-directory-not-exist"));
+  } else if !_new_path.is_dir() {
+    return Err(tauri::InvokeError::from("target-directory-not-directory"));
+  }
+  return Ok(());
 }
